298a06e598136cdf7710353297443b5a176a51af
Peter <pschoenbauer@gmail.com>
2023-04-30 21:05:18 -0400

    js event actions with pong

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+dodger.style.backgroundColor = "#FF69B4";
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLRight") {
+      moveDodgerRight();
+    }
+  });
+
